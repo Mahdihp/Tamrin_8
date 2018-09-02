564ecac6dba57de1588e0c8b43b0f4a2dba6915f
mahdi <mahdihp.devsc@gmail.com>
2018-09-02 19:03:58 +0430

    تمرین ۸ قسمت دوم قسمت ۳ ج

--- src/part_2/TestClass.java
+++ src/part_2/TestClass.java
@@ -1,9 +1,27 @@
 package part_2;
 
+import java.util.*;
+
 public class TestClass {
     public static void main(String[] args) {
 
 
 
     }
+
+    private static void swaped(Map<String,String> map) {
+        Set<String> keys = map.keySet();
+        Collection<String> values = map.values();
+        map.clear();
+
+
+        HashMap<String,String> newMap = new HashMap();
+
+        for(Map.Entry<String,String> entry : map.entrySet())
+            newMap.put(entry.getValue(), entry.getKey());
+
+        map.putAll(newMap);
+    }
+
+
 }
